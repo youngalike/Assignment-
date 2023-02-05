f807192e9f9d6372ba74a9935bbdd23a769245ae
Alike Chukwudi <67779173+youngalike@users.noreply.github.com>
2023-02-05 14:29:23 +0100

    Add files via upload

---
+++ Alike Chukwudi 2019364031 2.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr
+from time import sleep
+
+
+def take():
+    r = sr.Recognizer()
+
+    # Filename/path is your audio file
+    audioFile = sr.AudioFile("Recording.wav") 
+    with audioFile as source:
+        r.pause_threshold = 0.5
+        r.adjust_for_ambient_noise(source)
+        audio = r.record(source)
+        print("Dectecting speech...")
+        sleep(3)
+        print("Transcribing...")
+        
+
+    try:
+        query = r.recognize_google(audio, language='en-Us')
+        sleep(0.5)
+        print("\n")
+        print(query)
+        
+
+    except Exception as e:
+        print("An error occured. Please check your internet connection")
+
+        return "None"
+
+    return query
+
+query = take()
+sleep(1)
+print("* Transcribed Successfully , Quitting the program now *")
+sleep(2)
